943ecdf4b5f471ebdd56b5a72fb830ffdafab384
linggarelang <linggar.elangp@students.amikom.ac.id>
2025-06-17 11:06:37 +0700

    feat: Add Home components

---
+++ app/components/home/HomeMain.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import React, { JSX } from "react";
+import { motion } from "framer-motion";
+
+const HomeMain = (): JSX.Element => {
+    return (
+        <div className="w-full h-auto mb-10 px-7 lg:px-20">
+            <div className="w-full h-screen flex flex-col items-center justify-center">
+                <motion.h1
+                    initial={{ opacity: 0, y: -100 }}
+                    whileInView={{ opacity: 1, y: 0 }}
+                    transition={{ duration: .2, delay: 1.5, ease: 'easeInOut' }}
+                    viewport={{ once: false, amount: 0.5 }}
+                    className="text-7xl font-normal tracking-widest text-black"
+                >
+                    PEJUANG
+                </motion.h1>
+            </div>
+        </div>
+    );
+};
+
+export default HomeMain;
---
+++ app/data/productLists.ts
@@ -0,0 +1,25 @@
+import { Products } from "@/app/types/products";
+
+export const products: Products[] = [
+    {
+        productId: 1,
+        name: "Product 1",
+        description: "Description for Product 1",
+        price: 100,
+        image: "/bg-image-pejuang.jpg",
+    },
+    {
+        productId: 2,
+        name: "Product 2",
+        description: "Description for Product 2",
+        price: 200,
+        image: "/bg-image-pejuang.jpg",
+    },
+    {
+        productId: 3,
+        name: "Product 3",
+        description: "Description for Product 3",
+        price: 300,
+        image: "/bg-image-pejuang.jpg",
+    },
+]
